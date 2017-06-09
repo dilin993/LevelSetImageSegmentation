50c17881d845d5c54518214f51a47a5ec2602205
Dilin <Dilin@DESKTOP-TO8V8SE>
2017-06-09 16:58:02 +0530

    Implemented gamma correction

--- LevelSetImageSegmentation/main.cpp
+++ LevelSetImageSegmentation/main.cpp
@@ -6,10 +6,68 @@ using namespace std;
 using namespace cv;
 
 const string DISPLAY_WINDOW = "Display Window";
+const string TRACKBAR_GAMMA = "Gamma Correction";
+int gamma;
+const int GAMMA_MAX = 100;
 const int INNER_ITER = 5;
 const int OUTER_ITER = 20;
 const double sigma = 0.8;
 const double c0 = 2;
+Mat img, imgC;
+
+void GammaCorrection(Mat& src, Mat& dst, float fGamma)
+{
+	CV_Assert(src.data);
+
+	// accept only char type matrices
+	CV_Assert(src.depth() != sizeof(uchar));
+
+	// build look up table
+	unsigned char lut[256];
+	for (int i = 0; i < 256; i++)
+	{
+		lut[i] = saturate_cast<uchar>(pow((float)(i / 255.0), fGamma) * 255.0f);
+	}
+
+	dst = src.clone();
+	const int channels = dst.channels();
+	switch (channels)
+	{
+	case 1:
+	{
+
+		MatIterator_<uchar> it, end;
+		for (it = dst.begin<uchar>(), end = dst.end<uchar>(); it != end; it++)
+			//*it = pow((float)(((*it))/255.0), fGamma) * 255.0;
+			*it = lut[(*it)];
+
+		break;
+	}
+	case 3:
+	{
+
+		MatIterator_<Vec3b> it, end;
+		for (it = dst.begin<Vec3b>(), end = dst.end<Vec3b>(); it != end; it++)
+		{
+
+			(*it)[0] = lut[((*it)[0])];
+			(*it)[1] = lut[((*it)[1])];
+			(*it)[2] = lut[((*it)[2])];
+		}
+
+		break;
+
+	}
+	}
+}
+
+void on_trackbar(int, void*)
+{
+	float gammaf = 3.0*(float)gamma / (float)GAMMA_MAX;
+	GammaCorrection(img, imgC, gammaf);
+	cout << "Gamma: " << gammaf << endl;
+	imshow(DISPLAY_WINDOW, imgC);
+}
 
 int main(int argc, char *argv[])
 {
@@ -19,7 +77,15 @@ int main(int argc, char *argv[])
 		return -1;
 	}
 
-	Mat img,imgSmooth,dx,dy,f,g,dx2,dy2;
+	img = imread(argv[1], cv::ImreadModes::IMREAD_GRAYSCALE);
+	namedWindow(DISPLAY_WINDOW, WINDOW_AUTOSIZE);
+	imshow(DISPLAY_WINDOW, img);
+	createTrackbar(TRACKBAR_GAMMA, DISPLAY_WINDOW, &gamma, GAMMA_MAX, on_trackbar);
+	on_trackbar(gamma, 0);
+	waitKey(0);
+	return 0;
+
+	/*Mat img,imgSmooth,dx,dy,f,g,dx2,dy2;
 	DRLSE_Edge drlse;
 
 	img = imread(argv[1], cv::ImreadModes::IMREAD_GRAYSCALE);
@@ -57,5 +123,5 @@ int main(int argc, char *argv[])
 		cvWaitKey(1000);
 	}
 	cvWaitKey(0);
-	return 0;
+	return 0;*/
 }
